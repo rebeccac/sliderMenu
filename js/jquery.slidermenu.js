904a2d752bf74ebf69c3bfcd396c67289721691d
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-05-21 19:27:08 +0930

    Fixed Firefox bug

--- js/jquery.slidermenu.js
+++ js/jquery.slidermenu.js
@@ -1,12 +1,24 @@
+/*
+sliderMenu v1.0.0
+jQuery Sliding Menu
+http://www.slidermenu.com
+Developed by Rebecca Cordingley http://github.com/rebeccac
+MIT license
+ */
+
+
 ;(function($) {
 
 	var defaults = {
+		fitToSize: "false",
 		title: "SliderMenu",
-		backgroundColor: "#403b33",
+		menuBgColor: "#403b33",
 		menuItemTitles: ["Home", "About", "Source Code", "GitHub", "Contact"],
 		menuItemIcons: [],
-		menuItemLinks: ["index.html", "about.html", "source.html", "#", "contact.html"],
+		menuItemLinks: ["index.html", "about.html", "source.html", "github.com", "contact.html"],
+		menuItemBgColor: [],
 		pageTitle: "SliderMenu",
+		pageLink: "http://www.slidermenu.com",
 		containerClass: "slidermenu"
 	};
 
@@ -17,8 +29,8 @@
 		this.element = element;
 		this.init();
 
-		$(".sliderButton").click( function() {
-			event.preventDefault();
+		$(".sliderButton").click( function(e) {
+			e.preventDefault();
 			$("#slider").toggleClass("slidermenu-left").toggleClass("slidermenu-pushed");
 			$('body').toggleClass("page-body-left").toggleClass("page-body-pushed");
 		});
@@ -27,23 +39,37 @@
 	Slidermenu.prototype.init = function() {
 		$('body').addClass("page-body page-body-left");
 
-		this.element.addClass(this.config.containerClass).addClass("slidermenu-left").css("background-color", this.config.backgroundColor);
+		this.element.addClass(this.config.containerClass).addClass("slidermenu-left").css("background-color", this.config.menuBgColor);
+
+		if (this.config.fitToSize == "true" || this.config.fitToSize == "True") {
+			this.element.css({
+				"height": "auto",
+				"padding-bottom": "60px"
+			});
+		}
+		var background = $("<div>", {
+			class: "background"
+		}).appendTo(this.element);
 
 		var nav = $("<nav>", {
 			id: "navigation"
-		}).appendTo(this.element);
+		}).appendTo(background);
 
 		var ul = $("<ul>").appendTo(nav);
-		
+
 		var x;
 		for (x = 0; x < this.config.menuItemTitles.length; x++) {
-			
+
 			var li = $("<li>").appendTo(ul);
-			
+			if ( this.config.menuItemBgColor.length > 0 ) {
+				li.addClass("item" + x);
+				$(".item"+x).css("background-color", this.config.menuItemBgColor[x]);
+			}
+
 			var menuItemLink = $("<a>", {
 				href: this.config.menuItemLinks[x],
 			}).appendTo(li);
-			
+
 			if ( this.config.menuItemIcons.length > 0 ) {
 
 				var menuIcons = $("<i>", {
@@ -56,6 +82,7 @@
 			}).appendTo(menuItemLink).addClass("menu-item");
 
 		}
+
 
 		var additionalinfo = $("<div>", {
 				class: "additional-info",
